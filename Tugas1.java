be7cf2daf185e6b2ef5f794ce577cf03465a0f32
PraktikumDaspro <email@ristapolinema03.com>
2025-10-30 09:02:07 +0700

    Tugas1

---
+++ Tugas1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Tugas1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai N: ");
+        int N = sc.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            int jumlah = 0;
+            System.out.print("\nN =" + i + "-> jumlah kuadrat =");
+
+            for (int j = 1; j <= i; j++) {
+                jumlah += j * j;
+                System.out.print(j * j);
+                if (j < i) {
+                    System.out.print(" + ");
+                }
+            }
+            System.out.print(" = " + jumlah);
+        }
+        sc.close();
+    }
+}
